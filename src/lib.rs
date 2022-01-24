757cef449a756278ac03963a5d83d573e145f4ce
CrepeGoat <studentoflife96@gmail.com>
2022-01-24 18:23:38 -0500

    testing `read_unary1`

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
 #[cfg(test)]
 mod tests {
+    use bitstream_io::read::BitRead;
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_bitstream_unary0() {
+        let source = [0b01000000_u8];
+        let mut bits = bitstream_io::read::BitReader::endian(&source[..], bitstream_io::BigEndian);
+        println!("{:?}", bits.read_unary1());
+        println!("{:?}", bits.read_unary1());
+        println!("{:?}", bits.read_unary1());
+
+        assert!(false);
     }
 }
