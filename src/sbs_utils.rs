869280ad43e00654e789e547a9cab797e2003e53
CrepeGoat <studentoflife96@gmail.com>
2022-02-03 22:32:32 -0500

    refactored `sbs_utils`

--- src/sbs_utils.rs
+++ src/sbs_utils.rs
@@ -23,7 +23,7 @@ const fn masked_bit(bits: u64, index: usize) -> bool {
     (bits & (1_u64 << index)) != 0
 }
 
-mod encoder_utils {
+mod encode {
     use super::*;
 
     #[inline]
@@ -35,17 +35,11 @@ mod encoder_utils {
     pub(super) const fn suffix_len(flen: usize) -> usize {
         flen - 2
     }
-}
 
-pub(crate) struct Encoder<W: BitWrite>(W);
-
-impl<W: BitWrite> Encoder<W> {
-    pub(crate) fn write(
-        &mut self,
+    pub(crate) fn write<W: BitWrite>(
+        bitstream: &mut W,
         value: NonZeroU64,
     ) -> Result<(), IncompleteIntError<NonZeroU64>> {
-        use encoder_utils::*;
-
         let value = value.get();
         let flen = 8 * core::mem::size_of::<u64>() - (value.leading_zeros() as usize);
 
@@ -57,41 +51,41 @@ impl<W: BitWrite> Encoder<W> {
 
         // Write vlen prefix
         for sublen in 0..vlen_prefix {
-            self.0.write_bit(true).map_err(|e| {
+            bitstream.write_bit(true).map_err(|e| {
                 (
                     e,
-                    IncompleteInt::Unbounded(decoder_utils::from_partial_length_indicator(sublen)),
+                    IncompleteInt::Unbounded(decode::from_partial_length_indicator(sublen)),
                 )
             })?;
         }
 
-        self.0.write_bit(false).map_err(|e| {
+        bitstream.write_bit(false).map_err(|e| {
             (
                 e,
-                IncompleteInt::Unbounded(decoder_utils::from_partial_length_indicator(vlen_prefix)),
+                IncompleteInt::Unbounded(decode::from_partial_length_indicator(vlen_prefix)),
             )
         })?;
 
         // Write next bit
-        self.0.write_bit(vlen_next_bit).map_err(|e| {
+        bitstream.write_bit(vlen_next_bit).map_err(|e| {
             (
                 e,
                 IncompleteInt::Bounded(
-                    decoder_utils::from_full_length_indicator(vlen_prefix),
-                    NonZeroUsize::new(1 + decoder_utils::suffix_len(vlen_prefix, true)).unwrap(),
+                    decode::from_full_length_indicator(vlen_prefix),
+                    NonZeroUsize::new(1 + decode::suffix_len(vlen_prefix, true)).unwrap(),
                 ),
             )
         })?;
 
         // Write suffix bits
         for sublen in (0..suffix_len).rev() {
-            self.0
+            bitstream
                 .write_bit(masked_bit(suffix_bits, sublen))
                 .map_err(|e| {
                     (
                         e,
                         IncompleteInt::Bounded(
-                            decoder_utils::from_full_prefix(
+                            decode::from_full_prefix(
                                 vlen_prefix,
                                 vlen_next_bit,
                                 (suffix_bits, suffix_len - sublen),
@@ -104,21 +98,21 @@ impl<W: BitWrite> Encoder<W> {
 
         assert_eq!(
             value,
-            decoder_utils::from_full(vlen_prefix, vlen_next_bit, suffix_bits).get()
+            decode::from_full(vlen_prefix, vlen_next_bit, suffix_bits).get()
         );
         Ok(())
     }
 
-    pub(crate) fn write_inf(&mut self) -> std::io::Error {
+    pub(crate) fn write_inf<W: BitWrite>(bitstream: &mut W) -> std::io::Error {
         loop {
-            if let Err(e) = self.0.write_bit(true) {
+            if let Err(e) = bitstream.write_bit(true) {
                 return e;
             }
         }
     }
 }
 
-mod decoder_utils {
+mod decode {
     use super::*;
 
     #[inline]
@@ -173,17 +167,13 @@ mod decoder_utils {
 
         NonZeroU64::new(flen_prefix_bits | suffix_bits).unwrap()
     }
-}
 
-pub(crate) struct Decoder<R: BitRead>(R);
-
-impl<R: BitRead> Decoder<R> {
-    pub(crate) fn read(&mut self) -> Result<NonZeroU64, IncompleteIntError<NonZeroU64>> {
-        use decoder_utils::*;
-
+    pub(crate) fn read<R: BitRead>(
+        bitstream: &mut R,
+    ) -> Result<NonZeroU64, IncompleteIntError<NonZeroU64>> {
         // Get prefixing ones stream
         let mut vlen_prefix = 0;
-        while self.0.read_bit().map_err(|e| {
+        while bitstream.read_bit().map_err(|e| {
             (
                 e,
                 IncompleteInt::Unbounded(from_partial_length_indicator(vlen_prefix)),
@@ -193,7 +183,7 @@ impl<R: BitRead> Decoder<R> {
         }
 
         // Get first literal digit bit -> determines result's MSBs
-        let first_digit = self.0.read_bit().map_err(|e| {
+        let first_digit = bitstream.read_bit().map_err(|e| {
             (
                 e,
                 IncompleteInt::Bounded(
@@ -207,7 +197,7 @@ impl<R: BitRead> Decoder<R> {
 
         let mut suffix_bits: u64 = 0;
         for sublen in 0..suffix_len {
-            let bit = self.0.read_bit().map_err(|e| {
+            let bit = bitstream.read_bit().map_err(|e| {
                 (
                     e,
                     IncompleteInt::Bounded(
@@ -230,71 +220,68 @@ mod tests {
     use proptest::*;
     use rstest::*;
 
-    use decoder_utils::*;
-
     proptest! {
         #[test]
         fn test_from_partial_length_indicator(min_vlen_prefix in 0_usize..64) {
             assert!(
-                from_partial_length_indicator(min_vlen_prefix)
-                    .contains(&from_partial_length_indicator(min_vlen_prefix).start)
+                decode::from_partial_length_indicator(min_vlen_prefix)
+                    .contains(&decode::from_partial_length_indicator(min_vlen_prefix).start)
             );
         }
 
         #[test]
         fn test_from_full_length_indicator(vlen_prefix in 0_usize..63) {
-            let result = from_full_length_indicator(vlen_prefix);
+            let result = decode::from_full_length_indicator(vlen_prefix);
 
             assert_eq!(
                 result.start(),
-                &from_partial_length_indicator(vlen_prefix).start,
+                &decode::from_partial_length_indicator(vlen_prefix).start,
             );
             assert_eq!(
                 result.end().get(),
-                from_partial_length_indicator(vlen_prefix+1).start.get() - 1,
+                decode::from_partial_length_indicator(vlen_prefix+1).start.get() - 1,
             );
         }
 
         #[test]
         fn test_from_full_prefix(vlen_prefix in 0_usize..62) {
-            let result1 = from_full_prefix(vlen_prefix, false, (0, 0));
-            let result2 = from_full_prefix(vlen_prefix, true, (0, 0));
+            let result1 = decode::from_full_prefix(vlen_prefix, false, (0, 0));
+            let result2 = decode::from_full_prefix(vlen_prefix, true, (0, 0));
 
             assert_eq!(
                 result1.start(),
-                from_full_length_indicator(vlen_prefix).start(),
+                decode::from_full_length_indicator(vlen_prefix).start(),
             );
             assert_eq!(result1.end().get(), result2.start().get() - 1);
             assert_eq!(
                 result2.end().get(),
-                from_full_length_indicator(vlen_prefix+1).start().get() - 1,
+                decode::from_full_length_indicator(vlen_prefix+1).start().get() - 1,
             );
         }
     }
 
     #[rstest(stream, _read_len, expt_result,
-        case(&[0b00111111][..], 2, Ok(NonZeroU64::new(3).unwrap())),
-        case(&[0b01011111][..], 3, Ok(NonZeroU64::new(4).unwrap())),
-        case(&[0b01111111][..], 3, Ok(NonZeroU64::new(5).unwrap())),
-        case(&[0b10001111][..], 4, Ok(NonZeroU64::new(6).unwrap())),
-        case(&[0b10011111][..], 4, Ok(NonZeroU64::new(7).unwrap())),
-        case(&[0b10100111][..], 5, Ok(NonZeroU64::new(8).unwrap())),
-        case(&[0b10111111][..], 5, Ok(NonZeroU64::new(11).unwrap())),
-        case(&[0b11000011][..], 6, Ok(NonZeroU64::new(12).unwrap())),
-        case(&[0b11001111][..], 6, Ok(NonZeroU64::new(15).unwrap())),
-        case(&[0b11010001][..], 7, Ok(NonZeroU64::new(16).unwrap())),
-        case(&[0b11011111][..], 7, Ok(NonZeroU64::new(23).unwrap())),
-        case(&[0b11100000][..], 8, Ok(NonZeroU64::new(24).unwrap())),
-        case(&[0b11100111][..], 8, Ok(NonZeroU64::new(31).unwrap())),
+        case([0b00111111], 2, Ok(NonZeroU64::new(3).unwrap())),
+        case([0b01011111], 3, Ok(NonZeroU64::new(4).unwrap())),
+        case([0b01111111], 3, Ok(NonZeroU64::new(5).unwrap())),
+        case([0b10001111], 4, Ok(NonZeroU64::new(6).unwrap())),
+        case([0b10011111], 4, Ok(NonZeroU64::new(7).unwrap())),
+        case([0b10100111], 5, Ok(NonZeroU64::new(8).unwrap())),
+        case([0b10111111], 5, Ok(NonZeroU64::new(11).unwrap())),
+        case([0b11000011], 6, Ok(NonZeroU64::new(12).unwrap())),
+        case([0b11001111], 6, Ok(NonZeroU64::new(15).unwrap())),
+        case([0b11010001], 7, Ok(NonZeroU64::new(16).unwrap())),
+        case([0b11011111], 7, Ok(NonZeroU64::new(23).unwrap())),
+        case([0b11100000], 8, Ok(NonZeroU64::new(24).unwrap())),
+        case([0b11100111], 8, Ok(NonZeroU64::new(31).unwrap())),
     )]
     fn test_read(
-        stream: &[u8],
+        stream: [u8; 1],
         _read_len: usize,
         expt_result: Result<NonZeroU64, IncompleteInt<NonZeroU64>>,
     ) {
-        let bitstream = BitReader::<_, BigEndian>::new(stream);
-        let mut reader = Decoder(bitstream);
-        let calc_result = reader.read().map_err(|(_e, partial_int)| partial_int);
+        let mut bitstream = BitReader::<_, BigEndian>::new(&stream[..]);
+        let calc_result = decode::read(&mut bitstream).map_err(|(_e, partial_int)| partial_int);
         assert_eq!(calc_result, expt_result);
     }
 
@@ -320,10 +307,10 @@ mod tests {
         expt_result: Result<(), IncompleteInt<NonZeroU64>>, // <- need to use `InputStream` for immutabile static references
     ) {
         let mut stream = [0xFF_u8];
-        let bitstream = BitWriter::<_, BigEndian>::new(&mut stream[..]);
-        let mut writer = Encoder(bitstream);
-        let calc_result = writer.write(value).map_err(|(_e, partial_int)| partial_int);
-        writer.write_inf();
+        let mut bitstream = BitWriter::<_, BigEndian>::new(&mut stream[..]);
+        let calc_result =
+            encode::write(&mut bitstream, value).map_err(|(_e, partial_int)| partial_int);
+        encode::write_inf(&mut bitstream);
         assert_eq!(calc_result, expt_result);
         assert_eq!(&stream[..], expt_stream);
     }
