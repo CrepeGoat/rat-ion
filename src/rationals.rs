0406f563ff80c08077ee2e20b4f011f674457138
CrepeGoat <studentoflife96@gmail.com>
2023-06-08 01:20:20 -0600

    added encode/decode_n for n=16, 32, 64

--- src/rationals.rs
+++ src/rationals.rs
@@ -101,6 +101,108 @@ pub fn decode_c8(bits: &u8) -> (u64, NonZeroU64) {
     )
 }
 
+pub fn encode_c16(numerator: u64, denominator: NonZeroU64) -> Result<u16, u16> {
+    let mut bits: [u8; size_of::<u16>()] = [0, 0];
+    let mut bitstream = BitEncoder::new(&mut bits[..]);
+    let mut coder = Coder::default();
+    let mut is_truncated: bool = false;
+
+    for value in rational64_to_cf(numerator, denominator) {
+        if coder.write(&mut bitstream, value).is_err() {
+            is_truncated = true;
+            break;
+        }
+    }
+    coder.write_inf(&mut bitstream);
+
+    let result = u16::from_be_bytes(bits);
+    if is_truncated {
+        Err(result)
+    } else {
+        Ok(result)
+    }
+}
+
+pub fn decode_c16(bits: &u16) -> (u64, NonZeroU64) {
+    let bits = bits.to_be_bytes();
+    let bitstream = BitDecoder::new(&bits[..]);
+    let coder = Coder::default();
+    cf_to_rational64(
+        map_to_complete_cf(coder.read_iter(bitstream))
+            .collect::<Vec<_>>()
+            .into_iter()
+            .rev(),
+    )
+}
+
+pub fn encode_c32(numerator: u64, denominator: NonZeroU64) -> Result<u32, u32> {
+    let mut bits: [u8; size_of::<u32>()] = [0, 0, 0, 0];
+    let mut bitstream = BitEncoder::new(&mut bits[..]);
+    let mut coder = Coder::default();
+    let mut is_truncated: bool = false;
+
+    for value in rational64_to_cf(numerator, denominator) {
+        if coder.write(&mut bitstream, value).is_err() {
+            is_truncated = true;
+            break;
+        }
+    }
+    coder.write_inf(&mut bitstream);
+
+    let result = u32::from_be_bytes(bits);
+    if is_truncated {
+        Err(result)
+    } else {
+        Ok(result)
+    }
+}
+
+pub fn decode_c32(bits: &u32) -> (u64, NonZeroU64) {
+    let bits = bits.to_be_bytes();
+    let bitstream = BitDecoder::new(&bits[..]);
+    let coder = Coder::default();
+    cf_to_rational64(
+        map_to_complete_cf(coder.read_iter(bitstream))
+            .collect::<Vec<_>>()
+            .into_iter()
+            .rev(),
+    )
+}
+
+pub fn encode_c64(numerator: u64, denominator: NonZeroU64) -> Result<u64, u64> {
+    let mut bits: [u8; size_of::<u64>()] = [0, 0, 0, 0, 0, 0, 0, 0];
+    let mut bitstream = BitEncoder::new(&mut bits[..]);
+    let mut coder = Coder::default();
+    let mut is_truncated: bool = false;
+
+    for value in rational64_to_cf(numerator, denominator) {
+        if coder.write(&mut bitstream, value).is_err() {
+            is_truncated = true;
+            break;
+        }
+    }
+    coder.write_inf(&mut bitstream);
+
+    let result = u64::from_be_bytes(bits);
+    if is_truncated {
+        Err(result)
+    } else {
+        Ok(result)
+    }
+}
+
+pub fn decode_c64(bits: &u64) -> (u64, NonZeroU64) {
+    let bits = bits.to_be_bytes();
+    let bitstream = BitDecoder::new(&bits[..]);
+    let coder = Coder::default();
+    cf_to_rational64(
+        map_to_complete_cf(coder.read_iter(bitstream))
+            .collect::<Vec<_>>()
+            .into_iter()
+            .rev(),
+    )
+}
+
 #[cfg(test)]
 mod tests {
     use std::collections::HashSet;
@@ -110,7 +212,7 @@ mod tests {
 
     #[test]
     fn test_c8_decode_encode_inverse() {
-        let encodings: Vec<_> = (0_u8..=0xFF).collect();
+        let encodings: Vec<_> = (u8::MIN..=u8::MAX).collect();
         let decodings = encodings.iter().map(|&byte| decode_c8(&byte));
         let encodings2: Vec<_> = decodings
             .map(|(numerator, denominator)| encode_c8(numerator, denominator).unwrap_or_else(|x| x))
@@ -121,29 +223,138 @@ mod tests {
 
     #[test]
     fn test_decode_c8_uniqueness() {
-        let encodings = 0_u8..=0xFF;
+        let encodings = u8::MIN..=u8::MAX;
         let mut decodings = encodings.map(|byte| decode_c8(&byte));
 
-        let mut duplicates = std::collections::HashSet::new();
+        let mut duplicates = HashSet::new();
         assert!(decodings.all(|decoding| duplicates.insert(decoding)));
     }
 
     #[test]
-    fn test_decode_c8_denominator_11_completeness() {
+    fn test_c8_denominator_11_completeness() {
         const LARGEST_COVERED_DENOMINATOR: u64 = 11;
-        let encodings = 0_u8..=0xFF;
-        let decodings: HashSet<_> = encodings.map(|byte| decode_c8(&byte)).collect();
 
         for denom in 1..=LARGEST_COVERED_DENOMINATOR {
             for numer in 1..denom {
                 if gcd(numer, denom) != 1 {
                     continue;
                 }
-                assert!(decodings.contains(&(numer, NonZeroU64::new(denom).unwrap())));
+                assert!(encode_c8(numer, NonZeroU64::new(denom).unwrap()).is_ok());
             }
             println!("denominator {:?} covered!", denom);
         }
     }
+
+    #[test]
+    fn test_c16_decode_encode_inverse() {
+        let encodings: Vec<_> = (u16::MIN..=u16::MAX).collect();
+        let decodings = encodings.iter().map(|&word| decode_c16(&word));
+        let encodings2: Vec<_> = decodings
+            .map(|(numerator, denominator)| {
+                encode_c16(numerator, denominator).unwrap_or_else(|x| x)
+            })
+            .collect();
+
+        assert_eq!(encodings, encodings2);
+    }
+
+    #[test]
+    fn test_decode_c16_uniqueness() {
+        let encodings = u16::MIN..=u16::MAX;
+        let mut decodings = encodings.map(|byte| decode_c16(&byte));
+
+        let mut duplicates = HashSet::new();
+        assert!(decodings.all(|decoding| duplicates.insert(decoding)));
+    }
+
+    #[test]
+    fn test_c16_denominator_125_completeness() {
+        const LARGEST_COVERED_DENOMINATOR: u64 = 125;
+
+        for denom in 1..=LARGEST_COVERED_DENOMINATOR {
+            for numer in 1..denom {
+                if gcd(numer, denom) != 1 {
+                    continue;
+                }
+                assert!(encode_c16(numer, NonZeroU64::new(denom).unwrap()).is_ok());
+            }
+            println!("denominator {:?} covered!", denom);
+        }
+    }
+
+    // #[test]
+    // fn test_c32_decode_encode_inverse() {
+    //     let encodings: Vec<_> = (u32::MIN..=u32::MAX).collect();
+    //     let decodings = encodings.iter().map(|&word| decode_c32(&word));
+    //     let encodings2: Vec<_> = decodings
+    //         .map(|(numerator, denominator)| {
+    //             encode_c32(numerator, denominator).unwrap_or_else(|x| x)
+    //         })
+    //         .collect();
+
+    //     assert_eq!(encodings, encodings2);
+    // }
+
+    // #[test]
+    // fn test_decode_c32_uniqueness() {
+    //     let encodings = u32::MIN..=u32::MAX;
+    //     let mut decodings = encodings.map(|byte| decode_c32(&byte));
+
+    //     let mut duplicates = HashSet::new();
+    //     assert!(decodings.all(|decoding| duplicates.insert(decoding)));
+    // }
+
+    // #[test]
+    // fn test_c32_denominator_13859_completeness() {
+    //     const LARGEST_COVERED_DENOMINATOR: u64 = 13859;
+
+    //     for denom in 1..=LARGEST_COVERED_DENOMINATOR {
+    //         for numer in 1..denom {
+    //             if gcd(numer, denom) != 1 {
+    //                 continue;
+    //             }
+    //             assert!(encode_c32(numer, NonZeroU64::new(denom).unwrap()).is_ok());
+    //         }
+    //         println!("denominator {:?} covered!", denom);
+    //     }
+    // }
+
+    // #[test]
+    // fn test_c64_decode_encode_inverse() {
+    //     let encodings: Vec<_> = (u64::MIN..=u64::MAX).collect();
+    //     let decodings = encodings.iter().map(|&word| decode_c64(&word));
+    //     let encodings2: Vec<_> = decodings
+    //         .map(|(numerator, denominator)| {
+    //             encode_c64(numerator, denominator).unwrap_or_else(|x| x)
+    //         })
+    //         .collect();
+
+    //     assert_eq!(encodings, encodings2);
+    // }
+
+    // #[test]
+    // fn test_decode_c64_uniqueness() {
+    //     let encodings = u64::MIN..=u64::MAX;
+    //     let mut decodings = encodings.map(|byte| decode_c64(&byte));
+
+    //     let mut duplicates = HashSet::new();
+    //     assert!(decodings.all(|decoding| duplicates.insert(decoding)));
+    // }
+
+    // #[test]
+    // fn test_c64_denominator_13859_completeness() {
+    //     const LARGEST_COVERED_DENOMINATOR: u64 = 13859;
+
+    //     for denom in 1..=LARGEST_COVERED_DENOMINATOR {
+    //         for numer in 1..denom {
+    //             if gcd(numer, denom) != 1 {
+    //                 continue;
+    //             }
+    //             assert!(encode_c64(numer, NonZeroU64::new(denom).unwrap()).is_ok());
+    //         }
+    //         println!("denominator {:?} covered!", denom);
+    //     }
+    // }
 
     #[test]
     fn test_gcd() {
